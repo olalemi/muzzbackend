d7d23ad04622048225b78b91adb38cf16ca0c0ea
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-11-09 15:49:29 +0000

    updated db syntax

--- database.ts
+++ database.ts
@@ -4,6 +4,8 @@ import "dotenv/config";
 export const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.mongoURI}`, {});
+    console.log("connected to db");
   } catch (err) {
+    console.log(err, "mongo fail");
   }
 };
